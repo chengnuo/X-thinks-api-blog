01b20044ae04c63d173208028025051776cc3f82
xjc <2585441871@qq.com>
2017-03-13 19:11:38 +0800

    不知道什么鬼。头晕先提交

--- src/api/controller/user.js
+++ src/api/controller/user.js
@@ -11,15 +11,19 @@ export default class extends Base {
     //auto render template file index_index.html
     let myPost = this.post();
 
-    let userData = await this.model('user')
+    let userInfo = await this.model('user')
             .where({
                 email: myPost.email,
                 password:myPost.password
             })
             .find();
-    console.log(userData)
-    if(!think.isEmpty(userData)){
-        return this.success()
+    console.log(userInfo)
+
+    if(!think.isEmpty(userInfo)){
+        await this.session('userInfo', userInfo);
+        return this.success({
+            "userInfo":userInfo
+        })
     }else{
         return this.fail()
     }
